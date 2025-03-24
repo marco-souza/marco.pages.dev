3fc0914e8890e672e38d566e22c5716cc4bc08e3
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-24 19:09:13 -0300

    fea: setup playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
-    exclude: ["dist", "node_modules", ".wrangler"],
+    exclude: ["dist", "node_modules", ".wrangler", "e2e"],
   },
 });
