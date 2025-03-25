ebb1734c9696dc28904fad68523db16a9cfcc0b5
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-25 14:17:28 -0300

    fix: add viewport

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -19,6 +19,7 @@ export default jsxRenderer(({ children }) => {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {title ? <title>{title}</title> : <></>}
 
