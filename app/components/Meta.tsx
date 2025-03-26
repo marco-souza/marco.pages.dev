c5de6e481601790b037b3f2a5bb30cdd16f74602
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-26 08:54:09 -0300

    debug: use full paths to images

--- app/components/Meta.tsx
+++ app/components/Meta.tsx
@@ -3,6 +3,9 @@ import { configs } from "@/constants";
 const { description, images, pageURL, title } = configs.site;
 
 export function Meta() {
+  const ogImagePath = `${pageURL}${images.og}`;
+  const logoImagePath = `${pageURL}${images.logo}`;
+  const iconImagePath = `${pageURL}${images.icon}`;
   return (
     <>
       <meta charset="UTF-8" />
@@ -12,28 +15,26 @@ export function Meta() {
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <link rel="icon" type="image/svg+xml" href={images.icon} />
+      <link rel="icon" type="image/svg+xml" href={iconImagePath} />
       <meta name="robots" content="index, follow" />
 
       {/* Google / Search Engine Tags */}
       <meta itemProp="name" content={title} />
       <meta itemProp="description" content={description} />
-      <meta itemProp="image" content={images.og} />
+      <meta itemProp="image" content={ogImagePath} />
 
       {/* Facebook Meta Tags */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={pageURL} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:logo" content={images.logo} />
-      <meta property="og:image" itemProp="image" content={images.og} />
+      <meta property="og:logo" content={logoImagePath} />
+      <meta property="og:image" itemProp="image" content={ogImagePath} />
       <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="300" />
-      <meta property="og:image:hegiht" content="300" />
       <meta
         property="og:image:secure_url"
         itemProp="image"
-        content={images.og}
+        content={ogImagePath}
       />
 
       {/* Twitter Meta Tags */}
@@ -41,7 +42,7 @@ export function Meta() {
       <meta name="twitter:site" content={pageURL} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={images.og} />
+      <meta name="twitter:image" content={ogImagePath} />
     </>
   );
 }
