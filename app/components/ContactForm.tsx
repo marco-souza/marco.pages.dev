0369cf7728d52592c91663a069e10658254f9dc3
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-27 10:24:56 -0300

    fea: add a contact form

---
+++ app/components/ContactForm.tsx
@@ -0,0 +1,98 @@
+import type { FC } from "hono/jsx";
+
+interface ContactFormProps {
+  errors: {
+    name?: string;
+    email?: string;
+    message?: string;
+  };
+  values: {
+    name?: string;
+    email?: string;
+    message?: string;
+  };
+}
+
+export const ContactForm: FC<ContactFormProps> = ({ errors, values }) => {
+  return (
+    <div class="max-w-md mx-auto mt-10 p-6 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold text-center mb-4">Contact Us</h2>
+      <form method="post">
+        <div class="mb-4">
+          <label htmlFor="name" class="block text-sm font-medium">
+            Name
+          </label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={values.name}
+            class={`input input-bordered w-full ${errors.name ? "input-error" : ""}`}
+            placeholder="Your Name"
+          />
+          {errors.name && (
+            <p class="text-red-500 text-xs mt-1">{errors.name}</p>
+          )}
+        </div>
+
+        <div class="mb-4">
+          <label htmlFor="email" class="block text-sm font-medium">
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={values.email}
+            class={`input input-bordered w-full ${errors.email ? "input-error" : ""}`}
+            placeholder="Your Email"
+          />
+          {errors.email && (
+            <p class="text-red-500 text-xs mt-1">{errors.email}</p>
+          )}
+        </div>
+
+        <div class="mb-4">
+          <label htmlFor="message" class="block text-sm font-medium">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            class={`textarea textarea-bordered w-full ${errors.message ? "textarea-error" : ""}`}
+            placeholder="Your Message"
+          >
+            {values.message}
+          </textarea>
+          {errors.message && (
+            <p class="text-red-500 text-xs mt-1">{errors.message}</p>
+          )}
+        </div>
+
+        <button type="submit" class="btn btn-primary w-full">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+interface SuccessFormProps {
+  message: string;
+}
+
+export const ContactFormSuccess: FC<SuccessFormProps> = ({ message }) => {
+  return (
+    <div class="max-w-md mx-auto mt-10 p-6 bg-green-100 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold text-center mb-4 text-green-800">
+        Message sent! 🎉
+      </h2>
+      <p class="text-center text-green-700">{message}</p>
+      <div class="flex justify-center mt-4">
+        <a href="/" class="btn btn-outline btn-success">
+          Go Home
+        </a>
+      </div>
+    </div>
+  );
+};
---
+++ app/routes/contact/index.tsx
@@ -0,0 +1,67 @@
+import { z } from "zod";
+import { Hono } from "hono";
+import { ContactForm, ContactFormSuccess } from "#/app/components/ContactForm";
+
+const app = new Hono<{ Bindings: Cloudflare.Env }>();
+
+app.get("/", async (c) => {
+  const values = {};
+  const errors = {};
+  return c.render(<ContactForm values={values} errors={errors} />);
+});
+
+const ContactDataValidator = z.object({
+  name: z.string().nonempty("Name is required"),
+  email: z.string().email("Invalid email address"),
+  message: z.string().nonempty("Message is required"),
+});
+
+// receive user email, name and message
+app.post(
+  "/",
+  // TODO: validate for data
+  // zValidator("form", ContactDataValidator),
+  async (c) => {
+    const body = await c.req.parseBody();
+    const data = ContactDataValidator.safeParse(body);
+
+    if (!data.success) {
+      const errors = {
+        name: "",
+        email: "",
+        message: "",
+      };
+
+      for (const err of data.error.errors) {
+        if (err) {
+          if (err.path.includes("name")) {
+            errors.name = err.message;
+          }
+
+          if (err.path.includes("email")) {
+            errors.email = err.message;
+          }
+
+          if (err.path.includes("message")) {
+            errors.message = err.message;
+          }
+        }
+      }
+
+      return c.render(<ContactForm values={body} errors={errors} />);
+    }
+
+    // TODO: persist or send email
+    console.log(data.data);
+
+    return c.redirect("/contact/success");
+  },
+);
+
+app.get("/success", async (c) => {
+  return c.render(
+    <ContactFormSuccess message="Your contact information has been sent!" />,
+  );
+});
+
+export default app;
