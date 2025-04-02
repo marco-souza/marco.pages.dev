ce8a620b9f9e646f7ad7d098d2b900f7069520ee
Marco Antônio <ma.souza.junior@gmail.com>
2025-04-02 19:57:10 -0300

    fea: create fake dashboard page

---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,36 @@
+import { createRoute } from "honox/factory";
+import { getCookie, setCookie } from "hono/cookie";
+
+export const POST = createRoute(async (c) => {
+  const { name } = await c.req.parseBody<{ name: string }>();
+  setCookie(c, "name", name);
+  return c.redirect("/about");
+});
+
+export default createRoute((c) => {
+  const name = getCookie(c, "name") ?? "no name";
+
+  return c.render(
+    <div class="mx-auto justify-center w-xs">
+      <h1 class="text-2xl">Hello, {name ?? "people"}!</h1>
+      <form method="post">
+        <fieldset class="fieldset bg-base-200 border border-base-300 p-4 rounded-box">
+          <legend class="fieldset-legend">Name</legend>
+          <div class="join">
+            <input
+              type="text"
+              name="name"
+              class="input join-item"
+              placeholder="Your name"
+              value={name}
+              autofocus
+            />
+            <button type="submit" class="btn join-item">
+              save
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </div>,
+  );
+});
